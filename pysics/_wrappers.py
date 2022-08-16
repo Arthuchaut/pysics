7eeb54219a12620f9f95875ca0d900b9d87b56b6
Arthuchaut <arthur.audebrand@gmail.com>
2022-08-16 18:32:46 +0200

    Defined OpenGL properties wrappers

---
+++ pysics/_wrappers.py
@@ -0,0 +1,27 @@
+from typing import Final, TypeAlias
+from glfw.GLFW import *
+from OpenGL.GL import *
+
+
+class _GLWrapper:
+    viewport: Final[TypeAlias] = glViewport
+    matrix_mode: Final[TypeAlias] = glMatrixMode
+    load_indentity: Final[TypeAlias] = glLoadIdentity
+    ortho: Final[TypeAlias] = glOrtho
+    clear: Final[TypeAlias] = glClear
+    color_3f: Final[TypeAlias] = glColor3f
+    color_4f: Final[TypeAlias] = glColor4f
+    begin: Final[TypeAlias] = glBegin
+    end: Final[TypeAlias] = glEnd
+    vertex_2f: Final[TypeAlias] = glVertex2f
+
+
+class _GLFWWrapper:
+    init: Final[TypeAlias] = glfwInit
+    create_window: Final[TypeAlias] = glfwCreateWindow
+    get_frame_buffer_size: Final[TypeAlias] = glfwGetFramebufferSize
+    make_context_current: Final[TypeAlias] = glfwMakeContextCurrent
+    swap_buffers: Final[TypeAlias] = glfwSwapBuffers
+    window_should_close: Final[TypeAlias] = glfwWindowShouldClose
+    poll_events: Final[TypeAlias] = glfwPollEvents
+    terminate: Final[TypeAlias] = glfwTerminate
---
+++ tests/unit/test_wrappers.py
@@ -0,0 +1,43 @@
+from typing import Any, Callable
+import pytest
+from glfw.GLFW import *
+from OpenGL.GL import *
+from pysics._wrappers import _GLWrapper, _GLFWWrapper
+
+
+@pytest.mark.unit
+class TestGLWrapper:
+    def test_attrs(self) -> None:
+        attr_mapping: dict[str, Callable[..., Any]] = dict(
+            viewport=glViewport,
+            matrix_mode=glMatrixMode,
+            load_indentity=glLoadIdentity,
+            ortho=glOrtho,
+            clear=glClear,
+            color_3f=glColor3f,
+            color_4f=glColor4f,
+            begin=glBegin,
+            end=glEnd,
+            vertex_2f=glVertex2f,
+        )
+
+        for attr_name, exp_value in attr_mapping.items():
+            assert getattr(_GLWrapper, attr_name) == exp_value
+
+
+@pytest.mark.unit
+class TestGLFWWrapper:
+    def test_attrs(self) -> None:
+        attr_mapping: dict[str, Callable[..., Any]] = dict(
+            init=glfwInit,
+            create_window=glfwCreateWindow,
+            get_frame_buffer_size=glfwGetFramebufferSize,
+            make_context_current=glfwMakeContextCurrent,
+            swap_buffers=glfwSwapBuffers,
+            window_should_close=glfwWindowShouldClose,
+            poll_events=glfwPollEvents,
+            terminate=glfwTerminate,
+        )
+
+        for attr_name, exp_value in attr_mapping.items():
+            assert getattr(_GLFWWrapper, attr_name) == exp_value
